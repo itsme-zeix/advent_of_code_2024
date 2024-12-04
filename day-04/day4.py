3292b4710589b54b680d917de059dbb720c823ed
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-12-05 05:39:56 +0800

    Add day4

---
+++ day-04/day4.py
@@ -0,0 +1,48 @@
+def process_input():
+  with open("./day-04/input.txt", "r") as f:
+    matrix = f.read().splitlines()
+    rows = len(matrix)
+    cols = len(matrix[0])
+    return matrix, rows, cols
+
+def check_part1(x, y, letters, index, dx, dy):
+  if index == len(letters):
+    return 1
+  
+  nx, ny = x + dx, y + dy
+  if 0 <= nx < rows and 0 <= ny < cols:
+    if matrix[nx][ny] == letters[index]:
+      return check_part1(nx, ny, letters, index + 1, dx, dy)
+  return 0
+
+def solve_part1():
+  directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+  res = 0
+
+  for r in range(rows):
+    for c in range(cols):
+      if matrix[r][c] == 'X':
+        for dx, dy in directions:
+          res += check_part1(r, c, ['X', 'M', 'A', 'S'], 1, dx, dy)
+  return res
+
+def solve_part2():
+  res = 0
+  for r in range(rows):
+    for c in range(cols):
+      if matrix[r][c] == 'A':
+        diagonal1 = False
+        diagonal2 = False
+        if 0 <= r + 1 < rows and 0 <= r - 1 < rows and 0 <= c + 1 < cols and 0 <= c - 1 < cols:
+          diagonal1 = (matrix[r + 1][c + 1] == "M" and matrix[r - 1][c - 1] == "S") or \
+            (matrix[r + 1][c + 1] == "S" and matrix[r - 1][c - 1] == "M")
+          diagonal2 = (matrix[r + 1][c - 1] == "M" and matrix[r - 1][c + 1] == "S") or \
+            (matrix[r + 1][c - 1] == "S" and matrix[r - 1][c + 1] == "M")
+        if diagonal1 and diagonal2:
+          res += 1
+  return res
+
+if __name__ == "__main__":
+  matrix, rows, cols = process_input()
+  print(solve_part1())
+  print(solve_part2())
